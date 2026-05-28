e00dfc0391f4e1f2c2a6181b1b4bec63bc1b3afc
tunalee <dongwon1103@naver.com>
2026-05-28 13:55:40 +0900

    fix: remove unused SessionProvider, clarify AUTH_SECRET requirement

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 // app/layout.tsx
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import { SessionProvider } from "next-auth/react"
 import "./globals.css"
 
 const geistSans = Geist({
@@ -29,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionProvider>{children}</SessionProvider>
+        {children}
       </body>
     </html>
   )
